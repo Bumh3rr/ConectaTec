d0bcdf8a393e9465764a6a40ea5626f765cac79e
CrisGod7 <cristian2005barragan@gmail.com>
2026-04-30 14:27:53 -0600

    feat(dashboard): agregar modelos ActividadItem, DashboardStats, DocenteStats

---
+++ app/src/main/java/com/conectatec/model/DocenteStats.java
@@ -0,0 +1,11 @@
+package com.conectatec.model;
+
+public class DocenteStats {
+    public int misGrupos;
+    public int tareasActivas;
+    public int entregasHoy;
+    public int pendientesCalificar;
+    public int alumnosTotales;
+    public float promedioGeneral;
+    public int[] entregasPorDia;
+}
