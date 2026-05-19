beba8404fe048e4f2976e2280a01ed8b1a26a8fd
CrisGod7 <cristian2005barragan@gmail.com>
2026-05-19 10:47:34 -0600

    feat: aplicar tema DayNight desde Application.onCreate

--- app/src/main/java/com/conectatec/TecConnectApp.java
+++ app/src/main/java/com/conectatec/TecConnectApp.java
@@ -1,17 +1,14 @@
 package com.conectatec;
 
 import android.app.Application;
+import com.conectatec.ui.common.ThemePreferenceManager;
 import dagger.hilt.android.HiltAndroidApp;
 
-/**
- * Clase Application principal de TecConnect.
- * @HiltAndroidApp arranca el grafo de inyección de dependencias.
- * Registrar en AndroidManifest: android:name=".TecConnectApp"
- */
 @HiltAndroidApp
 public class TecConnectApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        ThemePreferenceManager.applyTheme(this);
     }
 }
